eef894c2cf71f9776969bfe2793a81378559ab63
ryan <ryan.c.schwiebert@gmail.com>
2015-03-01 03:09:23 -0500

    Specifying stdout as logging destination.

--- ringapp/settings.py
+++ ringapp/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -120,6 +121,7 @@ LOGGING = {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
             'formatter': 'simple',
+            'stream': sys.stdout
         },
         'dlogger_handler': {
             'level': 'DEBUG',
