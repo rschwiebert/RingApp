5d453753233664749fa84f59d92f8adac0be5235
rschwiebert <ryan.c.schwiebert@gmail.com>
2016-03-22 15:52:53 -0400

    Move main app to top of installed apps for proper inheritance

--- ringapp/settings.py
+++ ringapp/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'ringapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ringapp',
 )
 
 MIDDLEWARE_CLASSES = (
