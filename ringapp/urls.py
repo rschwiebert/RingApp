81bbfe14800823b77f3fff4cae4a99bfe4978c53
Ryan Schwiebert <ryanschwiebert@Ryans-MacBook-Pro.local>
2018-01-13 14:23:34 -0500

    import change in urls

--- ringapp/urls.py
+++ ringapp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import TemplateView, ListView, DetailView, FormView
